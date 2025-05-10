b59b4c41e4c75d12e1f01acd0fdff8193e3b043e
AnaliaMagallanes <167925588+AnaliaMagallanes@users.noreply.github.com>
2025-05-10 22:00:11 +0000

    remodelacion de estilo

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,21 +1,22 @@
 
 import React from "react";
-
-
+import { FaClock } from "react-icons/fa"; 
 function SecondsCounter(props) {
+  const segundos = props.seconds;
+
+  const digits = String(segundos).padStart(6, '0').split('');
+
   return (
-    <div className="text-center d-flex justify-content-center">
-    <p>{Math.floor(props.seconds/3600) % 60}</p>
-   
-    <p>{Math.floor(props.seconds/60) % 60}</p>
-    
-    <p>{Math.floor(props.seconds) % 60}</p>
-</div>
+    <div className="counter-container">
+      <div className="digit"><FaClock /></div>
+      {digits.map((digit, index) => (
+        <div key={index} className="digit">{digit}</div>
+      ))}
+    </div>
   );
 }
 
 export default SecondsCounter;
-
 
 
 
